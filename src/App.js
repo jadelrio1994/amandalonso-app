58129c9a9f89ccefe295df69b00048e4a570e898
Pikin <jadelrio1994@gmail.com>
2021-02-23 20:59:01 +0100

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './style/components/AppRouter'
 
 function App() {
   return (
     <div className="app__container">
-      <h1>Puto Sitio</h1>
+      <AppRouter />
     </div>
   );
 }
---
+++ src/style/components/AppRouter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AppRouter = () => {
+  return (
+    <div className="appRouter__container">
+      <h1>Router</h1>
+    </div>
+  )
+}
